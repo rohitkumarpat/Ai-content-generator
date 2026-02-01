90e1d271b4837e6a0fef46b37819f971458e3779
rohit singh <rohitsingh95083318@gmail.com>
2026-02-01 22:54:35 +0530

    eslint ignore add in nextconfig.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["cdn-icons-png.flaticon.com"], 
+    domains: ["cdn-icons-png.flaticon.com"],
+  },
+
+ 
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
